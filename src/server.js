519510e93043bd84140eb6dc42a3ac5b12ca9f99
yankvasya <yankvasya@gmail.com>
2021-10-01 14:17:47 +0300

    websocket-server renamed to server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const WebSocketServer = new require('ws');
+const Server = new require('ws');
 
 const clients = {};
 let currentId = 1;
 const port = 8999;
 
-const webSocketServer = new WebSocketServer.Server({port: port});
+const webSocketServer = new Server.Server({port: port});
 
 webSocketServer.on('connection', function(ws) {
     const id = currentId++;
